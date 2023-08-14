7f69a13950a7ee63329e5a2d10578ffa987df3d4
JptTheDeveloper <jpttheweeb@gmail.com>
2023-08-13 21:23:05 -0400

    running build?

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +14,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/happy-birthday' : '',
+		}
 	}
 };
 
