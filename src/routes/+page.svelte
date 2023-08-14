8099943670a30b6fa32024280016207987c0c656
JptTheDeveloper <jpttheweeb@gmail.com>
2023-08-13 21:54:08 -0400

    rough draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Typewriter, { loopOnce } from 'svelte-typewriter';
+</script>
+
+<Typewriter>
+    <h1>Happy Birthday Bluumarine!!</h1>
+</Typewriter>
+
+<Typewriter cursor={false} mode=loopOnce delay={2000}>
+    <h1>You are really cool. </h1>
+</Typewriter>
+
+<Typewriter cursor={false} mode=loopOnce delay={4000}>
+    <h1>From one overthinker, to another ...</h1>
+</Typewriter>
+
+<Typewriter cursor={false} mode=loopOnce delay={6000}>
+    <h1>I want you to know ... </h1>
+</Typewriter>
+
+<Typewriter cursor={false} mode=loopOnce delay={8000}>
+    <h1>You are so cool! :D </h1>
+</Typewriter>
+
+<Typewriter cursor={false} mode=loopOnce delay={10000}>
+    <h1>I really, really, really hope this year is super great for you. </h1>
+</Typewriter> 
+
